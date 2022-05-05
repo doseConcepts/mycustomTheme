27556673322a6f7e162f0e8f84c10a71fa4d3e99
DamnDose <endoseatwork@gmail.com>
2022-05-05 11:21:46 -0400

    updated php
    added header and footer php.

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>My Portfolio Homepage</title>
+    <!-- CSS Stylesheet-->
+    <link href="css/portfoliocss.css" rel="stylesheet">
+    <!-- JSS -->
+    <script type="text/javascript" src="../js/portfolio.js"></script>
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+      body {
+        background-color: #060709;
+      }
+    </style>
+    <?php wp_head();  ?>
+  </head>
+  <body <?php body_class(); ?>>
--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en-US">
-  <head>
-    <title>My Portfolio Homepage</title>
-    <link href="css/portfoliocss.css" rel="stylesheet">
-    <script type="text/javascript" src="../js/portfolio.js"></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-      body {
-        background-color: #060709;
-      }
-    </style>
-  </head>
-  <body>
+<?php get_header(); ?>
     <div id="grid">
       <nav class="navbar navbar-expand-sm navbar-light">
         <div class="container-fluid">
