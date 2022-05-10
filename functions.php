363463ef2c3017d5861cbb25058226d1bce8df67
DamnDose <endoseatwork@gmail.com>
2022-05-10 12:17:01 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ function enqueue_my_styles_and_scripts(){
   wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . '/css/portfoliocss.css', array(), '1.0.0', 'all');
 
 
-  wp_enqueue_script('my-scripts', get_themefile_uri() . '/js/portfoliojs.js', array('jquery'), '1.0.0', 'all');
+  wp_enqueue_script('my-scripts', get_themefile_uri() . '/js/portfoliojs.js', array(''), '1.0.0', 'all');
 
 }
 
